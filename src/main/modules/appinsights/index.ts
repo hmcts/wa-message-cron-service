7fe33ab5cfc65572eadab2ddaa5b55b473e12b7f
Ravi Aghera <Ravi.Aghera@HMCTS.net>
2023-10-09 14:42:56 +0100

    downgrade Chalk

--- src/main/modules/appinsights/index.ts
+++ src/main/modules/appinsights/index.ts
@@ -5,12 +5,9 @@ const appInsightsEnabled: boolean = config.get('appInsights.enabled');
 const appInsightsKey: string = config.get('appInsights.instrumentationKey');
 
 export class AppInsights {
-
   enable(): void {
     if (appInsightsEnabled && appInsightsEnabled === true) {
-      appInsights.setup(appInsightsKey)
-      .setSendLiveMetrics(true)
-      .start();
+      appInsights.setup(appInsightsKey).setSendLiveMetrics(true).start();
 
       appInsights.defaultClient.context.tags[appInsights.defaultClient.context.keys.cloudRole] =
         'wa-message-cron-service';
