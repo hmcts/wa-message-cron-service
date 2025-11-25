66b0981d7a06877bf3ed8a5eb5db644b8f7a9252
Aditya Dwadasi <131441305+adityadwadasi@users.noreply.github.com>
2025-11-25 07:47:19 +0000

    RWA-1696: Unit tests added for cron-service. (#763)
    * RWA-4006: Test routes fix
    
    * Updated Readme file
    
    * RWA-4006: Added tests to cron-service
    
    * Updated tests
    
    * Updated yarn.lock
    
    * Added dev dependencies
    
    * Reverted dependency update
    
    * Reverted secrets
    
    * Updated jest config
    
    * Updated yarn lock
    
    * Updated env variables
    
    * Updated test config
    
    * Resolved PR comments

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,11 @@
 module.exports = {
   roots: ['<rootDir>/src/test/unit'],
   testRegex: '(/src/test/.*|\\.(test|spec))\\.(ts|js)$',
-  moduleFileExtensions: ['ts', 'js'],
   testEnvironment: 'node',
-  transform: {
-    '^.+\\.ts?$': 'ts-jest',
-  },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  transformIgnorePatterns: ['/node_modules/(?!react-dnd|dnd-core|@react-dnd|react-dnd-html5-backend)'],
+  coverageProvider: 'v8',
+  transform: {
+    '^.+\\.(ts|tsx)$': ['ts-jest', { tsconfig: 'tsconfig.json' }],
+  },
 };
